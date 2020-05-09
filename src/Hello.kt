0c9579feac64f6913579b963f75ff44ab28f8b88
weenta <weenta@qq.com>
2020-05-10 06:27:48 +0800

    init pos homework

---
+++ src/Hello.kt
@@ -0,0 +1,62 @@
+fun main(args: Array<String>) {
+    val receipt = getReceipt()
+    println(receipt)
+    val result = when (receipt == expectedReceipt) {
+        true -> "正确 ✅"
+        false -> "错误 ❌"
+    }
+    println("\n结果：${result}")
+}
+
+interface Promotion {
+    var barcodes: List<String>
+}
+
+data class BuyTwoGetOneFreePromotion(override var barcodes: List<String>) : Promotion {
+
+}
+
+fun loadPromotions(): List<Promotion> =
+    listOf(BuyTwoGetOneFreePromotion(listOf("ITEM000000", "ITEM000001", "ITEM000005")))
+
+data class Item(val barcode: String, val name: String, val unit: String, val price: Double) {
+
+}
+
+fun loadAllItems(): List<Item> {
+    return listOf(
+        Item("ITEM000000", "可口可乐", "瓶", 3.00),
+        Item("ITEM000001", "雪碧", "瓶", 3.00),
+        Item("ITEM000002", "苹果", "斤", 5.50),
+        Item("ITEM000003", "荔枝", "斤", 15.00),
+        Item("ITEM000004", "电池", "个", 2.00),
+        Item("ITEM000005", "方便面", "袋", 4.50)
+    )
+}
+
+val purchasedBarcodes = listOf(
+    "ITEM000001",
+    "ITEM000001",
+    "ITEM000001",
+    "ITEM000001",
+    "ITEM000001",
+    "ITEM000003-2",
+    "ITEM000005",
+    "ITEM000005",
+    "ITEM000005"
+)
+
+fun getReceipt(): String {
+    return ""
+}
+
+const val expectedReceipt = """
+***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.0(元)
+名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.0(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.0(元)
+----------------------
+总计：51.00(元)
+节省：7.50(元)
+**********************
+"""
